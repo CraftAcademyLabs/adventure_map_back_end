65cca411c7246d679e51a7df02cc2ac6cad52a29
tochman <thomas@craftacademy.se>
2017-01-24 17:34:37 +0100

    fixes for failing tast appearing after merge with upstream

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -5,25 +5,24 @@ RSpec.describe 'User Registration', type: :request do
 
   context 'with valid credentials' do
     it 'returns a user and token' do
-      post '/api/v1/auth', params: { email: 'example@craftacademy.se',
-                                     password: 'password',
-                                     password_confirmation: 'password',
-                                     name: 'Bob',
-                                     nickname: 'Bobby',
-                                     image: 'image.png'
+      post '/api/v1/auth', params: {email: 'example@craftacademy.se',
+                                    password: 'password',
+                                    password_confirmation: 'password',
+                                    name: 'Bob',
+                                    nickname: 'Bobby',
+                                    image: 'image.png'
       }, headers: headers
-
       expect(response_json['status']).to eq 'success'
       expect(response.status).to eq 200
     end
 
     it 'persists a user to the database' do
-      post '/api/v1/auth', params: { email: 'example@example.se',
-                                     password: 'password',
-                                     password_confirmation: 'password',
-                                     name: 'Bob',
-                                     nickname: 'Bobby',
-                                     image: 'image.png'
+      post '/api/v1/auth', params: {email: 'example@example.se',
+                                    password: 'password',
+                                    password_confirmation: 'password',
+                                    name: 'Bob',
+                                    nickname: 'Bobby',
+                                    image: 'image.png'
       }, headers: headers
 
       expect(User.last.name).to eq 'Bob'
@@ -34,20 +33,20 @@ RSpec.describe 'User Registration', type: :request do
 
   context 'returns an error message when user submits' do
     it 'non-matching password confirmation' do
-      post '/api/v1/auth', params: {
-          email: 'example@craftacademy.se', password: 'password',
-          password_confirmation: 'wrong_password'
+      post '/api/v1/auth', params: {email: 'example@craftacademy.se',
+                                    password: 'password',
+                                    password_confirmation: 'wrong_password'
       }, headers: headers
 
       expect(response_json['errors']['password_confirmation'])
-          .to eq ["doesn't match Password"]
+          .to include 'doesn\'t match Password'
       expect(response.status).to eq 422
     end
 
     it 'an invalid email address' do
-      post '/api/v1/auth', params: {
-          email: 'example@craft', password: 'password',
-          password_confirmation: 'password'
+      post '/api/v1/auth', params: {email: 'example@craft',
+                                    password: 'password',
+                                    password_confirmation: 'password'
       }, headers: headers
 
       expect(response_json['errors']['email']).to eq ['is not an email']
@@ -55,13 +54,14 @@ RSpec.describe 'User Registration', type: :request do
     end
 
     it 'an already registered email' do
-      FactoryGirl.create(:user, email: 'example@craftacademy.se',
-                         password: 'password',
-                         password_confirmation: 'password')
+      create(:user,
+             email: 'example@craftacademy.se',
+             password: 'password',
+             password_confirmation: 'password')
 
-      post '/api/v1/auth', params: {
-          email: 'example@craftacademy.se', password: 'password',
-          password_confirmation: 'password'
+      post '/api/v1/auth', params: {email: 'example@craftacademy.se',
+                                    password: 'password',
+                                    password_confirmation: 'password'
       }, headers: headers
 
       expect(response_json['errors']['email']).to eq ['has already been taken']
