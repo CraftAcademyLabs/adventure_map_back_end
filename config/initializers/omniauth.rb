988051047a14b305ee7444aba0b09465da38356f
tochman <thomas@craftacademy.se>
2017-01-26 13:02:13 +0100

    removes comments

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :facebook, '1632349790403273', 'ad940a279de6e3030d94a6ff4fb28452'
 end
