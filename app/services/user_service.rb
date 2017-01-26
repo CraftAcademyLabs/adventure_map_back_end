4c2576f543d1b6e27c33c9a9a2cec0ab39f4fab9
tochman <thomas@craftacademy.se>
2017-01-26 08:45:56 +0100

    displayes profile picture on the user show page if there is one

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,7 +2,7 @@ class UserService
   include Godmin::Resources::ResourceService
 
   attrs_for_index :name, :nickname, :email, :created_at
-  attrs_for_show :name, :nickname, :email, :date_of_birth, :gender, :city, :created_at, :updated_at
+  attrs_for_show :image, :name, :nickname, :email, :date_of_birth, :gender, :city, :created_at, :updated_at
   attrs_for_form :name, :nickname, :email, :date_of_birth, :gender, :city, :password, :password_confirmation
 
   def resource_class
