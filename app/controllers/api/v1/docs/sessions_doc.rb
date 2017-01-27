8c71befb6a014b044762daf360131655f1a18ab2
Amber <amber@amberwilkie.com>
2017-01-27 16:56:10 +0100

    Include header in api docs for user login

--- app/controllers/api/v1/docs/sessions_doc.rb
+++ app/controllers/api/v1/docs/sessions_doc.rb
@@ -36,6 +36,25 @@ module Api::V1::Docs::SessionsDoc
                   "city":null
                 }
             }
+
+            Headers:
+            {
+              "X-Frame-Options"=>"SAMEORIGIN",
+              "X-XSS-Protection"=>"1; mode=block",
+              "X-Content-Type-Options"=>"nosniff",
+              "Content-Type"=>"application/json; charset=utf-8",
+              "access-token"=>"xXIwMG3yBOzxUF5S5CLGgg",
+              "token-type"=>"Bearer",
+              "client"=>"HcjE_K4u0TLWVQZ9UODHgw",
+              "expiry"=>"1486741920",
+              "uid"=>"email@email.com",
+              "Vary"=>"Origin",
+              "ETag"=>"W/\"3b5861d8ca94de88953745ad7e17e502\"",
+              "Cache-Control"=>"max-age=0, private, must-revalidate",
+              "X-Request-Id"=>"9de4307d-ddec-45b2-a387-55848662e38a",
+              "X-Runtime"=>"0.273076",
+              "Content-Length"=>"194"
+            }
   )
 
   def create
