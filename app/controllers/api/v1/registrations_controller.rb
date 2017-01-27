8e03732cf138397233ff9fb23ac320517ec68b3d
Amber <amber@amberwilkie.com>
2017-01-27 12:59:26 +0100

    WIP attempting to validate user through password

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -9,13 +9,15 @@ class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsControlle
   end
 
   def edit
-    @user.update(whitelist_params)
+    binding.pry
+    @user.update(whitelist_params) unless @user.nil?
     if @user.errors.empty?
       render json: {
           status: 'success',
           data: @user
       }
     else
+      @resource = @user
       render_update_error
     end
   end
@@ -24,23 +26,33 @@ class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsControlle
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit([:sign_up, :account_update, :edit], keys: [:name,
-                                                                :nickname,
-                                                                :image,
-                                                                :date_of_birth,
-                                                                :gender,
-                                                                :city])
+                                                                                 :nickname,
+                                                                                 :image,
+                                                                                 :date_of_birth,
+                                                                                 :gender,
+                                                                                 :city])
   end
 
   def authenticate_user
     @user = ::User.find_by(email: params['email'])
+    binding.pry
+    # I don't have access to @user.password - there has to be some smart API access token thing that could happen here so that users can be validated through the API. I can't find it.
+    if @user && @user.password == params['password']
+      @user
+    else
+      render json: {
+          status: 'error',
+          data: 'Invalid credentials'
+      }
+    end
   end
 
   def whitelist_params
     params.permit(:user, :name,
-                                :nickname,
-                                :image,
-                                :date_of_birth,
-                                :gender,
-                                :city)
+                  :nickname,
+                  :image,
+                  :date_of_birth,
+                  :gender,
+                  :city)
   end
 end
--- spec/requests/api/v1/edit_registrations_spec.rb
+++ spec/requests/api/v1/edit_registrations_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe 'Edit User Registration', type: :request do
     it 'returns a user with updates' do
       get '/api/v1/auth/edit', params: {email: user.email,
                                     password: user.password,
-                                    # password_confirmation: 'password',
                                     name: user.name,
                                     nickname: user.nickname,
                                     image: 'a_new_image.png'
@@ -20,11 +19,25 @@ RSpec.describe 'Edit User Registration', type: :request do
 
   context 'with invalid credentials' do
     it 'rejects an email that is not in the system' do
-
+      get '/api/v1/auth/edit', params: {email: 'fake@email.com',
+                                        password: user.password,
+                                        name: user.name,
+                                        nickname: user.nickname,
+                                        image: 'a_new_image.png'
+      }, headers: headers
+      expect(response_json['status']).to eq 'error'
+      expect(response_json['data']).to eq 'Invalid credentials'
     end
 
     it 'rejects the wrong password' do
-
+      get '/api/v1/auth/edit', params: {email: user.email,
+                                        password: 'wrong_password',
+                                        name: user.name,
+                                        nickname: user.nickname,
+                                        image: 'a_new_image.png'
+      }, headers: headers
+      expect(response_json['status']).to eq 'error'
+      expect(response_json['data']).to eq 'Invalid credentials'
     end
 
   end
