64873a9bf2edadd54baa15e6f92cdeab16173fe1
Amber Wilkie <amber@amberwilkie.com>
2017-01-30 12:00:58 +0100

    Users can edit their profile (#14)
    * Set up tests for editing a user profile
    
    * Add heroku link to readme
    
    * Create edit function for user registrations
    
    * WIP attempting to validate user through password
    
    * fixes specs for account update
    moves 'configure_permitted_parameters' back to application controller
    
    * removes before block in spec
    
    * refactors oauth stub and removes unused code in oauth controller
    
    * updates factory - mainly to kick of CircleCI
    
    * another update of factory - mainly to kick of CircleCI
    
    * refoctors 'configure_permitted_parameters' method in application controller
    
    * Update Api docs for registrations and sessions params
    
    * Remove binding.pry
    
    * Update headers for api docs, add description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception, unless: :json_request?
   protect_from_forgery with: :null_session, if: :json_request?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   def admin_user_class
@@ -31,6 +32,20 @@ class ApplicationController < ActionController::Base
 
   def json_request?
     request.format.json?
+  end
+
+  def configure_permitted_parameters
+    permitted_parameters = devise_parameter_sanitizer.instance_values['permitted']
+    attributes = [:name,
+                  :nickname,
+                  :image,
+                  :date_of_birth,
+                  :gender, :city]
+    attributes.each do |attribute|
+      permitted_parameters[:sign_up] << attribute
+      permitted_parameters[:account_update] << attribute
+    end
+
   end
 
 end
---
+++ spec/requests/api/v1/edit_registrations_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit User Registration', type: :request do
+  let!(:user) { FactoryGirl.create(:user, email: 'email@email.com', password: 'password') }
+  let(:headers) { {HTTP_ACCEPT: 'application/json'} }
+  let!(:invalid_auth_headers) { headers }
+  let!(:valid_auth_headers) { headers.merge(user.create_new_auth_token) }
+
+  context 'with valid headers' do
+    it 'returns a user with updates' do
+      put '/api/v1/auth',
+          params: {
+              name: 'Another Name',
+              image: 'a_new_image.png'
+          },
+          headers: valid_auth_headers
+
+      expect(response_json['status']).to eq 'success'
+      expect(response_json['data']['name']).to eq 'Another Name'
+      expect(response_json['data']['image']).to eq 'a_new_image.png'
+      expect(User.last.name).to eq 'Another Name'
+      expect(User.last.image).to eq 'a_new_image.png'
+    end
+  end
+
+  context 'with invalid headers' do
+    it 'sends error message' do
+      put '/api/v1/auth',
+          params: {
+              name: 'Another Name',
+              image: 'a_new_image.png'
+          },
+          headers: invalid_auth_headers
+
+      expect(response_json['status']).to eq 'error'
+      expect(response_json['errors']).to eq ['User not found.']
+    end
+  end
+
+end
