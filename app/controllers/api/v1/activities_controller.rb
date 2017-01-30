c8a45155620e7979a1e2064e41eb88f450698baa
Amber Wilkie <amber@amberwilkie.com>
2017-01-30 18:51:08 +0100

    User can create an activity - API endpoint (#17)
    * Expose API endpoint for creating an activity
    
    * Error handle a blank title
    
    * Properly handle user doesn't exist error
    
    * Take activity creation out of 'activity: {}' hash
    
    * Add activity creation API docs
    
    * Extract json rendering in activity creation to jbuilder views
    
    * Remove user_id from json response to activity creation

---
+++ app/controllers/api/v1/activities_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ActivitiesController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include Api::V1::Docs::ActivitiesDoc
+
+
+  def create
+    @activity = Activity.new(resource_params.merge(user: current_api_v1_user))
+    if @activity.save
+      render 'activities/show'
+    else
+      render 'activities/errors'
+    end
+
+  end
+
+  private
+
+  def resource_params
+    params.permit(:title, :body, :user_id, :difficulty, :category)
+  end
+end
