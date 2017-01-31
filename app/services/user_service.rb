5032f299157dbab8903b0976ad8fba7d745da5c4
sigu <sigumagwa@gmail.com>
2017-01-31 15:32:47 +0300

    remove unused method overides

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,23 +1,12 @@
+# frozen_string_literal: true
 class UserService
   include Godmin::Resources::ResourceService
 
   attrs_for_index :name, :nickname, :email, :created_at
-  attrs_for_form :name, :nickname, :email, :date_of_birth, :gender, :city, :interest_list, :password, :password_confirmation
-  attrs_for_show :image, :name, :nickname, :email, :interest_list, :date_of_birth, :gender, :city, :created_at, :updated_at
-
-  def resource_class
-    ::User
-  end
-
-  def build_resource(_params)
-    user = super
-    #user.setup_more_things
-    user
-  end
-
-  def create_resource(resource)
-    resource.save
-  end
+  attrs_for_form :name, :nickname, :email, :date_of_birth, :gender, :city,
+                 :interest_list, :password, :password_confirmation
+  attrs_for_show :image, :name, :nickname, :email, :interest_list,
+                 :date_of_birth, :gender, :city, :created_at, :updated_at
 
   def resource_class
     ::User
@@ -30,5 +19,4 @@ class UserService
     end
     resource.update(params)
   end
-
 end
