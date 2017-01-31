7f51e68d5b5d1ddeac6aea68c62b1d83e725335b
sigu <sigumagwa@gmail.com>
2017-01-31 14:37:40 +0300

    use interest parser while adding interest obeying rubocop :smile:

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
 Given(/^the user "([^"]*)" has an account$/) do |email|
   FactoryGirl.create(:user, email: email)
 end
 
 Given(/^the user "([^"]*)" has an interest "([^"]*)"$/) do |email, interest|
   user = User.find_by(email: email)
-  user.interest_list.add(interest)
+  # user the custom parser defined by us just to make sure only whitelisted
+  # interests are added
+  user.interest_list.add(interest, parser: InterestParser)
   user.save
 end
 
@@ -24,18 +27,17 @@ end
 
 And(/^I select date "([^"]*)" from Date of birth$/) do |date|
   page.execute_script(
-          "$('#user_date_of_birth').val('#{date}').trigger('keyup-change');"
+    "$('#user_date_of_birth').val('#{date}').trigger('keyup-change');"
   )
 end
 
-
 And(/^I fill in the form with basic credentials$/) do
-  steps %q{
+  steps '
     And I fill in "Email" with "user@random.com"
     And I fill in "Password" with "password"
     And I fill in "Password confirmation" with "password"
     And I select "Male" from "Gender"
     And I fill in "City" with "Gothenburg"
     And I select date "1971-11-24" from Date of birth
-        }
+        '
 end
