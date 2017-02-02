90789e400d666edf6f0886e8a6bdea555cded77d
Sigu Magwa <sigu@users.noreply.github.com>
2017-02-02 14:54:48 +0300

    Add predefined activity category (#21)
    * adjust category to select box
    * validate activity categories
    * remove comment
    * minor update to Gemfile to kick CI again

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,16 @@
 class Activity < ApplicationRecord
   VALID_DIFFICULTY_VALUES = [1, 2, 3]
+  VALID_CATEGORIES = User::VALID_INTERESTS
   validates_presence_of :title, :body, :difficulty
+  validate :validate_category
 
   validates :difficulty,
             inclusion: {in: VALID_DIFFICULTY_VALUES,
                         message: '%{value} is not a valid value'}
 
   belongs_to :user, dependent: :destroy
+
+  def validate_category
+    errors.add(:category) unless VALID_CATEGORIES.include? category
+  end
 end
--- spec/requests/api/v1/activity_spec.rb
+++ spec/requests/api/v1/activity_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Create Activity', type: :request do
   let(:headers) { {HTTP_ACCEPT: 'application/json'} }
   let!(:invalid_auth_headers) { headers }
   let!(:valid_auth_headers) { headers.merge(user.create_new_auth_token) }
+  let(:valid_category) { Activity::VALID_CATEGORIES.first }
 
   it 'creates a new Activity associated with a User' do
     post '/api/v1/activity',
@@ -12,14 +13,14 @@ RSpec.describe 'Create Activity', type: :request do
              title: 'An Amazing Time on the Slopes',
              body: 'Wow, I skiied a five diamond or whatever.',
              difficulty: 2,
-             category: 'Skiing'
+             category: valid_category
          },
          headers: valid_auth_headers
 
     expect(response_json['status']).to eq 'success'
     expect(response_json['activity']['title']).to eq 'An Amazing Time on the Slopes'
     expect(Activity.last.title).to eq 'An Amazing Time on the Slopes'
-    expect(Activity.last.category).to eq 'Skiing'
+    expect(Activity.last.category).to eq valid_category
   end
 
   it 'sends error message if there is no title' do
@@ -27,7 +28,7 @@ RSpec.describe 'Create Activity', type: :request do
          params: {
              body: 'Wow, I skiied a five diamond or whatever.',
              difficulty: 2,
-             category: 'Skiing'
+             category: valid_category
          },
          headers: valid_auth_headers
 
@@ -35,12 +36,26 @@ RSpec.describe 'Create Activity', type: :request do
     expect(response_json['message'][0]).to eq 'Title can\'t be blank'
   end
 
+  it 'sends error message if the category is not whitelisted' do
+    post '/api/v1/activity',
+         params: {
+             body: 'Wow, I skiied a five diamond or whatever.',
+             title: 'Nice title',
+             difficulty: 2,
+             category: 'Invalid Category'
+         },
+         headers: valid_auth_headers
+
+    expect(response_json['status']).to eq 'error'
+    expect(response_json['message'][0]).to eq 'Category is invalid'
+  end
+
   it 'sends error message if there is no body' do
     post '/api/v1/activity',
          params: {
              title: 'An Amazing Time on the Slopes',
              difficulty: 2,
-             category: 'Skiing'
+             category: valid_category
          },
          headers: valid_auth_headers
 
@@ -54,7 +69,7 @@ RSpec.describe 'Create Activity', type: :request do
              title: 'An Amazing Time on the Slopes',
              body: 'Wow, I skiied a five diamond or whatever.',
              difficulty: 2,
-             category: 'Skiing'
+             category: valid_category
          },
          headers: invalid_auth_headers
 
