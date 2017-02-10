a711204d72b1e6d34e05ffa0f34d8b4e0f353ca4
tochman <thomas@craftacademy.se>
2017-02-10 09:15:12 +0100

    fixes failing request spec

--- spec/requests/api/v1/show_activity_spec.rb
+++ spec/requests/api/v1/show_activity_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe 'Show Activity', type: :request do
 
   let!(:activity) { create(:activity, title: 'An Amazing Time on the Slopes') }
-  let! ( :activity_detail ) { create(:activity_detail, activity: activity) }
+  let!(:activity_detail) { create(:activity_detail, activity: activity, attachment_type: 'Image') }
 
   before(:each) do
     get "/api/v1/activities/#{activity.id}"
@@ -13,7 +13,7 @@ RSpec.describe 'Show Activity', type: :request do
   it 'Gets the correct activity' do
     expect(response_json['status']).to eq 'success'
     expect(response_json['data']['title'])
-      .to eq 'An Amazing Time on the Slopes'
+        .to eq 'An Amazing Time on the Slopes'
     expect(response_json['data']['body']).to eq activity.body
     expect(response_json['data']['difficulty']).to eq activity.difficulty
     expect(response_json['data']['category']).to eq activity.category
@@ -24,7 +24,7 @@ RSpec.describe 'Show Activity', type: :request do
   end
 
   it 'includes Activity details in response' do
-    expect(response_json['data']['activityDetails'][0]['activity_id'])
-    .to eq activity.id
+    expect(response_json['data']['images'][0]['activity_id'])
+        .to eq activity.id
   end
 end
