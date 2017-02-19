dcd646be0006383676f32a5dde4e461e34b95826
Raoul DIFFOUO <diraulo@gmail.com>
2017-02-19 23:45:06 +0200

    File upload to S3 (#32)
    * add api endpoint to generate presigned links to upload files to s3
    
    * minor refactoring to the file upload controller

---
+++ app/controllers/api/v1/file_upload_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::FileUploadController < ActionController::API
+  def upload
+    if valid_params?
+      params_obj = {
+        acl: 'public-read',
+        content_type: params[:content_type]
+      }
+
+      s3 = Aws::S3::Resource.new
+      obj = s3.bucket('adventuremap').object(filepath)
+      url = obj.presigned_url(:put, params_obj)
+
+      render json: { upload_url: url, public_url: obj.public_url }
+    else
+      render json: { error: 'Invalid Params' }
+    end
+  end
+
+  private
+
+  def valid_params?
+    params[:filename] && params[:type]
+  end
+
+  def filepath
+    "#{params[:type]}/#{params[:filename]}"
+  end
+end
