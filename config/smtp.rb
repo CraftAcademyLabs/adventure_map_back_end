fb3fc0abc21131b65dc95098ec35b07cfdb12a01
tochman <thomas@craftacademy.se>
2017-03-11 09:51:42 +0100

    disables mail settings - 3

--- config/smtp.rb
+++ config/smtp.rb
@@ -5,7 +5,8 @@ SMTP_SETTINGS = {
 #   enable_starttls_auto: ENV.fetch('SMTP_ENABLE_TLS'),
 #   password: ENV.fetch('SMTP_PASSWORD'),
 #   port: ENV.fetch('SMTP_PORT'),
-#   user_name: ENV.fetch('SMTP_USERNAME') }
+#   user_name: ENV.fetch('SMTP_USERNAME')
+}
 
 # if ENV['EMAIL_RECIPIENTS'].present?
 #   Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
