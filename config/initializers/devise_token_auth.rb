7805d5de02d51c2c9eac972ac647828242cdc4ff
tochman <thomas@craftacademy.se>
2017-03-13 07:18:35 +0100

    adds default_confirm_success_url setting in dta initializer to use env var if in prod

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -52,6 +52,11 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you can
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   config.enable_standard_devise_support = false
-  config.default_confirm_success_url = 'http://localhost'
+  if Rails.env.production?
+    config.default_confirm_success_url = ENV['HOST']
+  else
+    config.default_confirm_success_url = 'http://localhost'
+
+  end
 
 end
