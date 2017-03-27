9b48701c94f3ddf94d1d970a19c315cb6bd480c7
Amber <amber@amberwilkie.com>
2017-03-27 10:41:08 +0200

    Add Likes model, controller and success/error views

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::LikesController < ActionController::API
+  # include Api::V1::Docs::LikesDoc
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # Here "follow" means "like" - we are reusing gem functionality.
+
+  def create
+    activity = Activity.find(params[:activity_id])
+    if current_api_v1_user.follow activity
+      render 'success'
+    else
+      render 'error'
+    end
+  end
+
+  def destroy
+    activity = Activity.find(params[:activity_id])
+    if current_api_v1_user.stop_following activity
+      render 'success'
+    else
+      render 'error'
+    end
+  end
+end
