e1b034c6f12144646ee94c0846c385fcf913bfa4
Amber <amber@amberwilkie.com>
2017-03-27 13:31:00 +0200

    Add documentation for new endpoint

--- app/controllers/api/v1/docs/follows_doc.rb
+++ app/controllers/api/v1/docs/follows_doc.rb
@@ -1,6 +1,29 @@
 module Api::V1::Docs::FollowsDoc
   extend Apipie::DSL::Concern
 
+  api :GET, 'follows', 'Get a list of users following me or that I follow'
+  description "You need to provide a request: either 'followings' or 'followers'"
+  example %q(
+            Request:
+            {
+              "request":"followings",
+            }
+            Headers:
+            {
+              "access-token"=>"xXIwMG3yBOzxUF5S5CLG23lj",
+              "token-type"=>"Bearer",
+              "client"=>"HcjE_K4u0TLWVQZ9UODHgw",
+              "expiry"=>"1486741920",
+              "uid"=>"email@email.com",
+            }
+            "users": [{
+              "id": 1,
+              "name": "Tompa Thopmsson",
+              "image": null,
+              "interests": []
+            }],
+            )
+
   api :POST, '/follows', 'Follow a user '
   description 'You need to provide \'user_id\' for the user to be followed in the params with the request'
   example %q(
