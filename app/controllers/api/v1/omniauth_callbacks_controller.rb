25f77e1e70618f2facc582f05175dcb403c44e48
tochman <thomas@craftacademy.se>
2017-09-06 10:07:49 +0200

    removes forgotten pry...

--- app/controllers/api/v1/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
 
 
-
   def assign_provider_attrs(user, auth_hash)
     oauth_attrs = {
         first_name: auth_hash['info']['first_name'],
@@ -14,7 +13,6 @@ class Api::V1::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksC
     end
 
     unless user.nickname
-      binding.pry
       oauth_attrs.merge!({nickname: auth_hash['info']['first_name'].downcase})
     end
 
